23c4a8a630a816e6956dc2741594f1c4af745411
unknown <austinematurure@gmail.com>
2025-03-20 21:43:04 +0200

    feat: Added Login pagestyling

--- MobileApp/app/index.tsx
+++ MobileApp/app/index.tsx
@@ -7,75 +7,108 @@ import {
   Button,
   Image,
   Pressable,
+  TouchableOpacity,
 } from "react-native";
 import { Link } from "expo-router";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Talk {
-  Title: string;
-  Speaker: string;
+  title: string;
+  talk_info: {
+    speaker: string;
+  }[];
 }
 
-interface Schedule {
-  Date: string;
+interface ApplyPart {
+  apply_part: string;
+  apply_info: {
+    student: string;
+    duration: string;
+  }[];
+}
+
+interface BibleStudy {
+  title: string;
+  bible_study_info: {
+    conductor: string;
+    reader: string;
+  }[];
+}
+
+interface ScheduleSkel {
+  Schedule: {
+    id: string;
+    date: string;
+  };
   Opening: {
-    "Opening Song": string;
-    "Opening Prayer": string;
-    Chairman: string;
+    opening_song: string;
+    opening_prayer: string;
+    chairman: string;
   };
   Treasures: {
-    Talk: Talk;
-    "Spiritual Gems": Talk;
-    "Bible Reading": string;
+    bible_reading: string;
+    treasures_talks: Talk[];
   };
   "Apply Yourself": {
-    [key: string]: {
-      Student: string;
-      Duration: string;
-    };
+    apply_parts: ApplyPart[];
   };
   "Living as Christians": {
-    Song: string;
-    Talks: {
-      [key: string]: {
-        Speaker: string;
-        Duration?: string;
-        Conductor?: string;
-        Reader?: string;
-      };
-    };
+    living_song: string;
+    living_talks: {
+      title: string;
+      living_talk_info: {
+        speaker: string;
+        duration: string;
+      }[];
+    }[];
+    bible_study: BibleStudy[];
   };
   Closing: {
-    "Closing Song": string;
-    "Closing Prayer": string;
+    closing_song: string;
+    closing_prayer: string;
   };
 }
 
 export default function Index() {
-  const [data, setData] = useState<Schedule>();
+  const [data, setData] = useState<ScheduleSkel | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [currentWeekIndex, setCurrentWeekIndex] = useState<number>(0);
+  const [next, setNext] = useState<number | 0>(7);
+
+  const [currentWeekIndexDate, setCurrentDate] = useState<Date>(new Date());
+
+  const formattedDate = currentWeekIndexDate.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
 
   useEffect(() => {
     axios
-      .get(`http://192.168.0.31:8000/schedular/api/${currentWeekIndex}`)
+      .get(
+        `http://192.168.0.31:8000/schedular/api/getschedule/${currentWeekIndexDate.toLocaleString(
+          "en-GB",
+          { day: "2-digit", month: "long", year: "numeric" }
+        )}`
+      )
       .then((response) => {
-        setData(response.data);
+        if (response.status === 404) {
+          setNext(0);
+        } else {
+          setData(response.data);
+          setNext(7);
+          console.log(response.data);
+        }
       })
       .catch((err) => {
-        console.error("API call failed:", err);
         setError(err.message);
+        if (err.response && err.response.status === 404) {
+          setError(
+            err.response.data.error || "No data found for the requested date."
+          );
+        }
       });
-  }, [currentWeekIndex]);
-
-  if (error) {
-    return (
-      <View style={styles.center}>
-        <Text>{`Error: ${error}`}</Text>
-      </View>
-    );
-  }
+  }, [currentWeekIndexDate]);
 
   if (!data) {
     return (
@@ -85,18 +118,16 @@ export default function Index() {
     );
   }
 
-  const currentSchedule = data;
-
   const goToNextWeek = () => {
-    if (currentWeekIndex < 4) {
-      setCurrentWeekIndex(currentWeekIndex + 1);
-    }
+    const newDate = new Date(currentWeekIndexDate);
+    newDate.setDate(currentWeekIndexDate.getDate() + next);
+    setCurrentDate(newDate);
   };
 
   const goToPreviousWeek = () => {
-    if (currentWeekIndex > 0) {
-      setCurrentWeekIndex(currentWeekIndex - 1);
-    }
+    const newDate = new Date(currentWeekIndexDate);
+    newDate.setDate(currentWeekIndexDate.getDate() - next);
+    setCurrentDate(newDate);
   };
 
   const removeNumbers = (str: string) => {
@@ -115,29 +146,35 @@ export default function Index() {
   };
 
   return (
-    <SafeAreaView className=" gap-4">
-      <ScrollView className="flex-col gap-4 bg-zinc-800 ">
+    <SafeAreaView className="gap-4">
+      <ScrollView className="flex-col gap-4 bg-zinc-800">
         <View className="flex-row justify-center gap-6 items-center text-center p-6">
-          <Pressable onPress={goToPreviousWeek}>
+          <TouchableOpacity
+            onPress={goToPreviousWeek}
+            disabled={next == 0 ? true : false}
+            activeOpacity={next == 0 ? 0.6 : 0.9}
+          >
             <Image
               source={require("../assets/images/next-svgrepo-com (5).png")}
               className="w-9 h-9 object-cover flex-1"
               resizeMode="cover"
             />
-          </Pressable>
+          </TouchableOpacity>
           <View>
             <Text style={styles.dateText} className="text-white">
-              {currentSchedule.Date}
+              {data.Schedule.date}
             </Text>
           </View>
-          <Pressable onPress={goToNextWeek}>
+          <Pressable onPress={goToNextWeek} disabled={next == 0 ? true : false}>
             <Image
               source={require("../assets/images/next-svgrepo-com (4).png")}
               className="w-9 h-9 object-cover"
             />
           </Pressable>
         </View>
-        <View className="p-2 ">
+
+        {/* Your Schedule Link Section */}
+        <View className="p-2">
           <Link href={"./upcoming"} asChild>
             <Pressable>
               <View className="bg-lightblue rounded-3xl p-3 mb-2">
@@ -151,71 +188,47 @@ export default function Index() {
             </Pressable>
           </Link>
         </View>
+
+        {/* Opening Section */}
         <View className="rounded-t-3xl bg-lightwhite box-border p-6 z-10 gap-6 rounded-2xl">
-          {/* Opening Section */}
           <View className="gap-2">
             <View className="p-5 back-g rounded-xl flex-col">
               <Text className="font-bold mb-3">
-                {currentSchedule.Opening["Opening Song"]}
+                {data.Opening.opening_song}
               </Text>
               <Text>
                 <Text className="font-bold text-lg">Opening Prayer: </Text>
-
-                {currentSchedule.Opening["Opening Prayer"]}
+                {data.Opening.opening_prayer}
               </Text>
-
               <Text>
                 <Text className="font-bold text-lg">Chairman: </Text>
-
-                {currentSchedule.Opening.Chairman}
+                {data.Opening.chairman}
               </Text>
             </View>
           </View>
 
           {/* Treasures Section */}
           <View className="gap-2">
-            <Text style={styles.sectionTitle} className=" p-2 rounded-xl">
-              Treasures From God's Word{" "}
+            <Text style={styles.sectionTitle} className="p-2 rounded-xl">
+              Treasures From God's Word
             </Text>
-            <View className="p-4 flex-col gap-3 back-g rounded-2xl">
-              <View className="gap-2">
-                <View>
+            <View className="p-4 flex-col gap-3 back-g rounded-2xl ">
+              {data.Treasures.treasures_talks.map((talk, index) => (
+                <View key={index} className="gap-2 ">
                   <Text className="font-bold text-xl">
-                    {currentSchedule.Treasures.Talk.Title.trimStart()}
+                    {talk.title.trimStart()}
                   </Text>
+                  {talk.talk_info.map((info, infoIndex) => (
+                    <Text key={infoIndex}>{info.speaker}</Text>
+                  ))}
+                  {index < data.Treasures.treasures_talks.length - 1 && (
+                    <View className="border-b-hairline"></View>
+                  )}
                 </View>
-
-                <View className="inline">
-                  <Text className="">
-                    {currentSchedule.Treasures.Talk.Speaker}
-                  </Text>
-                </View>
-              </View>
-              <View className="border-b-hairline"></View>
-              <View className=" ">
-                <Text>
-                  <View className="gap-1">
-                    <View className="flex-row justify-between ">
-                      <Text className="font-bold text-xl">Spiritual Gems </Text>
-                      <Text className="font-bold text-xl">
-                        {removeGems(
-                          currentSchedule.Treasures["Spiritual Gems"].Title
-                        )}
-                      </Text>
-                    </View>
-                    <View></View>
-                    <View>
-                      <Text>
-                        {currentSchedule.Treasures["Spiritual Gems"].Speaker}
-                      </Text>
-                    </View>
-                  </View>
-                </Text>
-              </View>
-              <View className="border-b-hairline"></View>
-              <View className="flex-col gap-1">
-                <Text className="font-bold text-xl">Bible Reading</Text>
-                <Text>{currentSchedule.Treasures["Bible Reading"]}</Text>
+              ))}
+              <View className="flex-col gap-1 border-t-hairline ">
+                <Text className="font-bold text-xl mt-2">Bible Reading</Text>
+                <Text>{data.Treasures.bible_reading}</Text>
               </View>
             </View>
           </View>
@@ -226,81 +239,95 @@ export default function Index() {
               Apply Yourself To the Ministry
             </Text>
             <View className="flex-col gap-4">
-              {Object.entries(currentSchedule["Apply Yourself"]).map(
-                ([activity, details]) => (
-                  <View
-                    key={activity}
-                    className="flex-row justify-between items-center p-4  back-g rounded-2xl "
-                  >
-                    <View className="flex-col gap-1 ">
-                      <Text className="font-bold text-xl ">
-                        {removeNumbers(activity)}
-                      </Text>
-                      <Text className="text-[14px] ml-1">
-                        {details.Student}
-                      </Text>
-                    </View>
-                    <View>
-                      <Text className="bg-primary font-bold self-start rounded-xl text-lightwhite p-2">
-                        {removeParenteshis(details.Duration)}
-                      </Text>
-                    </View>
+              {data["Apply Yourself"].apply_parts.map((part, index) => (
+                <View
+                  key={index}
+                  className="flex-row justify-between items-center p-4 back-g rounded-2xl"
+                >
+                  <View className="flex-col gap-1 ">
+                    <Text className="font-bold text-xl">
+                      {removeNumbers(part.apply_part)}
+                    </Text>
+                    <Text className="text-[14px] ml-1">
+                      {part.apply_info[0].student}
+                    </Text>
                   </View>
-                )
-              )}
+                  <View>
+                    <Text className="bg-primary font-bold self-start rounded-xl text-lightwhite p-2">
+                      {removeParenteshis(part.apply_info[0].duration)}
+                    </Text>
+                  </View>
+                </View>
+              ))}
             </View>
           </View>
 
           {/* Living as Christians Section */}
           <View className="gap-2">
             <Text style={styles.sectionTitle}>Living as Christians</Text>
-            <View className=" flex-col p-4 gap-4 back-g rounded-2xl">
-              <Text>{currentSchedule["Living as Christians"].Song}</Text>
-              {Object.entries(
-                currentSchedule["Living as Christians"].Talks
-              ).map(([talk, details]) => (
-                <View key={talk} className="flex-col gap-4">
-                  {talk === "Congregation Bible Study" ? (
-                    <View className="flex-col">
-                      <View className="flex-row items-center justify-between">
-                        <Text className="font-bold text-xl blk">{talk}:</Text>
-                        <Text>{details.Conductor}</Text>
-                      </View>
-                      <View className="flex-row items-center justify-between">
-                        <Text className="font-bold text-xl">{`Reader: `}</Text>
-                        <Text>{details.Reader}</Text>
-                      </View>
-                    </View>
-                  ) : (
-                    <View className="flex-col  ">
-                      <View>
-                        <Text className="font-bold text-xl">{talk}</Text>
-                      </View>
+            <View className="flex-col p-4 gap-4 back-g rounded-2xl">
+              {/* Living Song */}
+              <Text>{data["Living as Christians"].living_song}</Text>
 
-                      <View className="flex-row justify-between items-center">
-                        <Text>{details.Speaker}</Text>
+              {/* Living Talks */}
+              {data["Living as Christians"].living_talks.map((talk, index) => (
+                <View key={index} className="flex-col gap-2">
+                  <Text className="font-bold text-xl">{talk.title}</Text>
+                  {talk.living_talk_info.map((info, infoIndex) => (
+                    <View
+                      key={infoIndex}
+                      className="flex-row justify-between items-center"
+                    >
+                      <Text>{info.speaker}</Text>
+                      {talk.title !== "Local Needs" ? (
                         <Text className="bg-primary font-bold self-start rounded-xl text-lightwhite p-2">
-                          {details.Duration &&
-                            `${removeParenteshis(details.Duration)}`}
+                          {info.duration &&
+                            info.duration !== " " &&
+                            `${removeParenteshis(info.duration)}`}
                         </Text>
-                      </View>
-                      <View className="border-b-hairline mt-3"></View>
+                      ) : null}
                     </View>
+                  ))}
+                  {index <
+                    data["Living as Christians"].living_talks.length - 1 && (
+                    <View className="border-b-hairline mt-1"></View>
                   )}
                 </View>
               ))}
+
+              {/* Bible Study Section */}
+              {
+                <View className="flex-col gap-3 border-t-hairline">
+                  {data[
+                    "Living as Christians"
+                  ].bible_study[0].bible_study_info.map((info, infoIndex) => (
+                    <View key={infoIndex} className="flex-col justify-between">
+                      <View className="flex-row justify-between items-center mt-4">
+                        <Text className="font-bold text-xl ">
+                          {"Congregation Bible Study:"}
+                        </Text>
+                        <Text>{info.conductor}</Text>
+                      </View>
+
+                      <View className="flex-row justify-between">
+                        <Text className="font-bold text-xl">Reader: </Text>
+                        <Text>{info.reader}</Text>
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              }
             </View>
           </View>
-
           {/* Closing Section */}
           <View>
             <View className="p-5 back-g rounded-xl flex-col">
               <Text className="font-bold text-lg">
-                {currentSchedule.Closing["Closing Song"]}
+                {data.Closing.closing_song}
               </Text>
               <View className="flex-row items-baseline">
                 <Text className="font-bold text-lg">Closing Prayer: </Text>
-                <Text>{currentSchedule.Closing["Closing Prayer"]}</Text>
+                <Text>{data.Closing.closing_prayer}</Text>
               </View>
             </View>
           </View>
--- MobileApp/app/upcoming.jsx
+++ MobileApp/app/upcoming.jsx
@@ -7,11 +7,65 @@ import {
   Button,
   Image,
   Pressable,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
 
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Upcoming() {
-  return <></>;
+  return (
+    <>
+      <View className="items-center h-screen  gap-6 p-6">
+        <View className="">
+          <View className="flex-col justify-between gap-6 mt-6">
+            <View>
+              <Text className="text-5xl font-bold text-lightblue">Create</Text>
+              <Text className="text-5xl font-bold"> an Account</Text>
+
+              <Text className="p-2">
+                Creating an allows you to see your upcoming parts and get
+                notified when a new Schedule is up.
+              </Text>
+            </View>
+
+            <View className=" flex-col   gap-4">
+              <View className="gap-2">
+                <TextInput
+                  className="text-2xl border-hairline rounded-md border-primary p-2"
+                  placeholder="Name"
+                ></TextInput>
+
+                <TextInput
+                  className="text-2xl border-hairline rounded-md border-primary p-2"
+                  placeholder="Surname"
+                ></TextInput>
+
+                <TextInput
+                  className="text-2xl border-hairline rounded-md border-primary p-2"
+                  secureTextEntry={true}
+                  placeholder="Password"
+                ></TextInput>
+              </View>
+              <View className="gap-2 mt-3">
+                <TouchableOpacity
+                  activeOpacity={0.64}
+                  className="bg-lightblue p-5 rounded-lg"
+                >
+                  <Text className="text-white text-center ">Sign Up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className="p-5 rounded-lg border-2 border-lightblue"
+                  activeOpacity={0.64}
+                >
+                  <Text className="text-lightblue  text-center">Log In</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
+  );
 }
